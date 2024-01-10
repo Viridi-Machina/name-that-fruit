2d9b35cc9acf4f4f70b0cb2a832fe57a2e91b2c8
jacen green <jacen.l.green@hotmail.com>
2024-01-10 13:32:27 +0000

    script: create questions arrays

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,3 +3,131 @@ All questions for use in the quiz. Arrays for each question including the answer
 well as an object containing the fruit name and image filepath are contained herein.
 */
 
+const quizArrayEasy = [
+    {
+        pictureCard: [''],
+        answers: ['Apple', 'Orange', 'Banana', 'Lemon', 3]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Plumb', 'Banana', 'Orange', 'Apple', 4]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Orange', 'Lime', 'Lemon', 'Grapefruit', 1]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Lime', 'Lemon', 'Orange', 'Apple', 2]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Plumb', 'Tomato', 'Orange', 'Peach', 4]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Lime', 'Lemon', 'Grapefruit', 'Banana', 1]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Grape', 'Lemon', 'Stripey Lime', 'Watermelon', 4]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Melon', 'Grape', 'Plumb', 'Orange', 2]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Apple', 'Tomato', 'Plumb', 'Peach', 3]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Lemon', 'Grapefruit', 'Orange', 'Peach', 2]
+    }
+];
+
+const quizArrayMedium = [
+    {
+        pictureCard: [''],
+        answers: ['Apple', 'Passionfruit', 'Lychee', 'Plumb', 2]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Papaya', 'Lemon', 'Orange', 'Lime', 1]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Mango', 'Orange', 'Peach', 'Grapefruit', 1]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Grape', 'Lychee', 'Passionfruit', 'Guava', 2]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Plumb', 'Apple', 'Orange', 'Tomato', 4]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Lime', 'Lemon', 'Pomegranate', 'Dragonfruit', 3]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Acai', 'Prickly Pear', 'Pinapple', 'Dragonfruit', 4]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Grape', 'Acai', 'Plumb', 'Passionfruit', 2]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Apple', 'Dragonfruit', 'Pineapple', 'Peach', 3]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Guava', 'Grapefruit', 'Grape', 'Mango', 1]
+    }
+];
+
+const quizArrayHard = [
+    {
+        pictureCard: [''],
+        answers: ['Papaya', 'Dragonfruit', 'Mangosteen', 'Pear', 3]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Plumb', 'Banana', 'Orange', 'Pear', 4]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Kiwi', 'Lime', 'Lemon', 'Grapefruit', 1]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Kiwi', 'Guava', 'Grape', 'Pear', 2]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Tamarillo', 'Nectarine', 'Tomato', 'Persimmon', 4]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Rambutan', 'Dragonfruit', 'Prickly Pear', 'Starfruit', 1]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Acai', 'Dragonfruit', 'Lemon', 'Starfruit', 4]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Melon', 'Tamarillo', 'Plumb', 'Nectarine', 2]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Apple', 'Blood Orange', 'Tomato', 'Peach', 3]
+    },
+    {
+        pictureCard: [''],
+        answers: ['Dragonfruit', 'Langsat', 'Lychee', 'Potato', 2]
+    }
+];
