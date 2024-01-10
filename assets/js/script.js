7e0397caad2d8d79920434d4ba269ddd9800bef5
jacen green <jacen.l.green@hotmail.com>
2024-01-10 11:56:42 +0000

    feat: create for loop to iterate through game selections, add hover and click effects

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,12 @@ let nameSubmit = document.getElementById('welcome-form');
 
 let resetButton = document.getElementById('reset-button');
 
+// Variables for setting display of each screen of the game.
 let startScreen = document.getElementById('welcome-screen');
 let difficultyScreen = document.getElementById('section-difficulty-select');
 let gameScreen = document.getElementById('quiz-screen');
 
-let 
+let selectDifficulty = document.getElementsByClassName('fruit');
 
 /**
  * This function returns the user to the difficulty screen when clicking the reset button.
@@ -19,8 +20,8 @@ function resetClicked(){
     startScreen.style.display = 'none';
     difficultyScreen.style.display = 'block';
     gameScreen.style.display = 'none';
-
 }
+
 /**
  * This function calls the difficulty screen after choosing a username, which
  * is then stored in local storage.
@@ -34,9 +35,37 @@ function difficultyScreenLoad(event){
     resetClicked();
 }
 
+// For loop to iterate through <div> objects for targetting
+for (var fruit of selectDifficulty){
+    //console.log(fruit);
+    var fruitHover = fruit.getElementsByTagName('h2');
+    console.log(fruitHover[0]);
+    fruitHover[0].addEventListener('mouseover', hoverSelect);
+    fruitHover[0].addEventListener('mouseout', hoverSelectOff);
+    fruitHover[0].addEventListener('click', runGame);
+}
+
+// The following two functions add a subtle hover transition effect.
+function hoverSelect(){
+    this.style.textShadow 
+    = '0px 0px 8px hsla(33, 67%, 80%, 1)';
+    this.style.transition = '0s';
+}
+function hoverSelectOff(){
+    this.style.textShadow 
+    = 'none';
+    this.style.transition = '0.5s';
+}
+
+console.log(fruitHover[0].getElementsByTagName('h2'))
 
 
-nameSubmit.addEventListener('submit', difficultyScreenLoad)
+function runGame(){
+    this.style.textDecoration = 'underline';
+}
+
+selectDifficulty[0].addEventListener('mouseover', hoverSelect);
+nameSubmit.addEventListener('submit', difficultyScreenLoad);
 resetButton.addEventListener('click', resetClicked);
 
-console.log('javascript load complete')
+console.log('javascript load complete');
